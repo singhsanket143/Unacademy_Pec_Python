69f4e69c4aff89d3c7c815a4f52ea0aaa404a266
Sanket Singh <singhsanket143@gmail.com>
2021-01-14 10:31:18 +0530

    Add files via upload

---
+++ specialcharacters.py
@@ -0,0 +1,24 @@
+# There exist multiple special or escape characters in python, some of them are
+
+# 1. quotes
+x = '\'' 	# this shows single quote inside single quote
+y = "\'" 	# this shows single quote inside double quote
+z = "\"" 	# this shows double quote inside double quote
+w = " ' " 	# this shows single quote inside double quote
+v = '"' 	# this shows double quote inside single quote
+print(x, y, z, w, v)
+
+# 2. New line
+n = 'sanket \n singh' # represents new line character
+print(n)
+
+# 3. Tab space
+t = 'sanket\tsingh' # represents tab character
+print(t)
+
+# 4. back slash
+b = 'sanket \\singh'
+print(b)
+
+# ??? What about rest??? we will learn on the go
+# note -> \ - backslash, / - forward slash
