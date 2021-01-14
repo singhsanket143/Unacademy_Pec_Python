b329559904978452a2495ea35d0b0d6284ddde71
Sanket Singh <singhsanket143@gmail.com>
2021-01-14 10:31:41 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,43 @@
+# 1. Arithmetic Operators
+x = 34
+y = 17
+
+print("ADDITION", x + y)
+print("SUBTRACTION", x - y)
+print("multiply", x * y)
+print("divide", x / y)
+print("floor divide", x // y)
+print("remainder", x % y)
+print("exponent", x ** y)
+
+
+print("---------------------------")
+# 2. Relational or Comparison Operator
+print("x Equals y", x == y)
+print("x not equals y", x != y)
+print("x less than or equal y", x <= y)
+print("x greater than or equal y", x >= y)
+print("x less than y", x < y)
+print("x greater than y", x > y)
+
+print("---------------------------")
+# 3. logical operator
+print("Use of and", (x != y) and (x >= y))
+print("Use of or", (x != y) or (x >= y))
+print("Use of not", not (x != y))
+
+print(not 2, not 0, not -4) 
+print(x and y) # returns x if x is false, otherwise y
+print(x or y) # returns y if x is false, otherwise x
+# positive int -> true, 0 -> false, negative -> true
+
+print(not "", not "abs")
+
+# empty string -> false, normal string -> true
+
+
+
+
+
+
+
